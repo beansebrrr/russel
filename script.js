1f846cdbe00400be4589d973ce15c5862a997dea
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-05-02 11:21:06 +0400

    ✨ Add breakout margins to content grid 🔤 Imported web fonts ✨ Added a horizontal scroller 🚚 Renamed `base/images/` to `base/assets/`

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("yep.");
+const horizontalScroller = document.querySelector("ul.horizontal-scroller")
+const cloneCard = () => horizontalScroller.querySelector("li").cloneNode(true)
+
+for (let i = 0; i < 8; i++) {
+  horizontalScroller.appendChild(cloneCard())
+}
