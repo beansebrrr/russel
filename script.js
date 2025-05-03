46b7300dfba636a6cee7563eb4b9f2c4cbda8a12
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-05-04 00:49:58 +0400

    ✨ Complete `Jewelry` page 🖼️ Uploaded images and populated pages with content

--- script.js
+++
@@ -1,6 +0,0 @@
-const horizontalScroller = document.querySelector("ul.horizontal-scroller")
-const cloneCard = () => horizontalScroller.querySelector("li").cloneNode(true)
-
-for (let i = 0; i < 8; i++) {
-  horizontalScroller.appendChild(cloneCard())
-}
